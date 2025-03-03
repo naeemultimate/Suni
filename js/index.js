2bb35868abb38dd492afc8c8b9d573aa710a7fc2
Naeem Shah <naeemultimate@gmail.com>
2025-03-03 15:36:35 +0000

    adjust for OSCE

--- js/index.js
+++ js/index.js
@@ -8,14 +8,14 @@
       closeB = $('close'),
       timer = null;
   console.log('wat', card);
-  openB.addEventListener('click', function () {
-    card.setAttribute('class', 'open-half');
-    if (timer) clearTimeout(timer);
-    timer = setTimeout(function () {
-      card.setAttribute('class', 'open-fully');
-      timer = null;
-    }, 1000);
-  });
+  // openB.addEventListener('click', function () {
+  //   card.setAttribute('class', 'open-half');
+  //   if (timer) clearTimeout(timer);
+  //   timer = setTimeout(function () {
+  //     card.setAttribute('class', 'open-fully');
+  //     timer = null;
+  //   }, 1000);
+  // });
 
   closeB.addEventListener('click', function () {
     card.setAttribute('class', 'close-half');
@@ -27,3 +27,18 @@
   });
 
 }());
+
+
+// VOLUME BUTTON
+document.addEventListener("DOMContentLoaded", function () {
+  const audio = document.getElementById("bg-music");
+  const volumeBtn = document.getElementById("volume-btn");
+
+  let isMuted = false;
+
+  volumeBtn.addEventListener("click", function () {
+      isMuted = !isMuted;
+      audio.muted = isMuted;
+      volumeBtn.innerHTML = isMuted ? "🔇 <span class='volume-text'>dhur</span>" : "🔊 <span class='volume-text'>dhur</span>";
+  });
+});
